b927274f3a0370a740d4acd7b1cd464b95b6f272
Cristi Cretu <crisemcr@gmail.com>
2021-10-04 20:45:13 +0300

    add new update

--- data/achievements.ts
+++ data/achievements.ts
@@ -1,82 +1,79 @@
 export const achievements = {
-    y2021: [
-        {
-            title: "Built this blog",
-            description: "Using NextJs, ReactJs, TailwindCSS, MDX",
-        },
-        {
-            title: "Earned my first internet money",
-            description: "",
-        },
-        {
-            title: "Started Freelancing",
-            description:
-                "I made landing pages using my tech stack (React + Tailwind) and UI/UX design.",
-        },
-        {
-            title: "First Tech Challenge Romania Dean's List Winner",
-            description:
-                "Selected as one of 330 students nationwide to represent Romania at the First Tech Challenge Dean's List Finalists event in Detroit in recognition of leadership and dedication for leading the team to increase  awareness for First and for achieving personal technical expertise and accomplishment. ",
-        },
-        {
-            title: "Qualified at the National Stage of the Olympiad of Mathematics",
-            description: "Organised by Gazeta Matematica - online",
-        },
-    ],
-    y2020: [
-        {
-            title: "Learned web development",
-            description: "Started with the basics: HTML, CSS, JS.",
-        },
-        {
-            title: "Participated in my first Hackaton",
-            description: "We made a C# application that placed us in top 30.",
-        },
-        {
-            title: "Restarted my life",
-            description:
-                "During the lockdown, I made a strong routine, increased productivity and quit social media. Started learning faster and focused more on programming.",
-        },
-    ],
-    y2019: [
-        {
-            title: "Joined a First Tech Challenge Robotics Team",
-            description: "",
-        },
-        {
-            title: "Entered High School",
-            description: "",
-        },
-        {
-            title: "Participated in the National Robotics Summer Camp",
-            description:
-                "I was a volunteer and I started a passion for robotics.",
-        },
-        {
-            title: "Learned to code - self taught",
-            description:
-                "At the end of 8th grade, I learned the basics of C++.",
-        },
-    ],
-    y2016: [
-        {
-            title: "First Internet Clients",
-            description:
-                "I made Minecraft Youtube Banners for Youtube Creators.",
-        },
-    ],
-    y2015: [
-        {
-            title: "Got my Second Computer",
-            description:
-                "This time I didn't just install viruses and games, instead I learned Photoshop, IoT and more about tech.",
-        },
-    ],
-    y2009: [
-        {
-            title: "First Computer",
-            description:
-                "I was fascinated by tech, probably the time where my passion started. I remember the nights full of games.",
-        },
-    ],
+  y2021: [
+    {
+      title: "Built this blog",
+      description: "Using NextJs, ReactJs, TailwindCSS, MDX",
+    },
+    {
+      title: "Earned my first internet money",
+      description: "",
+    },
+    {
+      title: "Started Freelancing",
+      description:
+        "I made landing pages using my tech stack (React + Tailwind) and UI/UX design.",
+    },
+    {
+      title: "First Tech Challenge Romania Dean's List Winner",
+      description:
+        "Selected as one of 330 students nationwide to represent Romania at the First Tech Challenge Dean's List Finalists event in Detroit in recognition of leadership and dedication for leading the team to increase awareness for First and for achieving personal technical expertise and accomplishment. ",
+    },
+    {
+      title: "Qualified at the National Stage of the Olympiad of Mathematics",
+      description: "Organised by Gazeta Matematica - online",
+    },
+  ],
+  y2020: [
+    {
+      title: "Learned web development",
+      description: "Started with the basics: HTML, CSS, JS.",
+    },
+    {
+      title: "Participated in my first Hackaton",
+      description: "We made a C# application that placed us in the top 30.",
+    },
+    {
+      title: "Restarted my life",
+      description:
+        "During the lockdown, I made a strong routine, increased productivity, and quit social media. Started learning faster and focused more on programming.",
+    },
+  ],
+  y2019: [
+    {
+      title: "Joined a First Tech Challenge Robotics Team",
+      description: "",
+    },
+    {
+      title: "Entered High School",
+      description: "",
+    },
+    {
+      title: "Participated in the National Robotics Summer Camp",
+      description: "I was a volunteer and I started a passion for robotics.",
+    },
+    {
+      title: "Learned to code - self taught",
+      description: "At the end of 8th grade, I learned the basics of C++.",
+    },
+  ],
+  y2016: [
+    {
+      title: "First Internet Clients",
+      description: "I made Minecraft Youtube Banners for Youtube Creators.",
+    },
+  ],
+  y2015: [
+    {
+      title: "Got my Second Computer",
+      description:
+        "This time I didn't just install viruses and games, instead I learned Photoshop, IoT, and more about tech.",
+    },
+  ],
+  y2009: [
+    {
+      title: "First Computer",
+      description:
+        "I was fascinated by tech, probably the time where my passion started. I remember the nights full of games.",
+    },
+  ],
 };
--- data/projects.ts
+++ data/projects.ts
@@ -1,104 +1,80 @@
 export const projects = [
   {
-    name: "Facebook",
-    description: "Front-End Facebook Clone using Firebase.",
-    github:
-      "https://github.com/cristicretu/learn-react/tree/main/02_facebook-clone",
-    tags: [
-      "nextjs",
-      "reactjs",
-      "javascript",
-      "tailwindcss",
-      "next-auth",
-      "firebase",
-      "API",
-    ],
+    name: "RoCovidTracker",
+    description:
+      "Covid-19 Tracker built using NextJs & TailwindCSS - data provided from geospatial.",
+    github: "https://github.com/cristicretu/covid-romania",
+    website: "https://covid.cretu.dev",
   },
   {
-    name: "iMessage",
+    name: "Learn React",
     description:
-      "iMessage Clone using the MERN (MongoDB, ExpressJS, ReactJs, NodeJs) Stack",
-    github: "https://github.com/cristicretu/iMessage-Clone",
-    tags: [
-      "reactjs",
-      "javascript",
-      "tailwindcss",
-      "mongodb",
-      "expressjs",
-      "firebase",
-    ],
+      "A personal app for learning ReactJs consisting of 6 big projects.",
+    github: "https://github.com/cristicretu/learn-react",
+  },
+  {
+    name: "Facebook Clone",
+    description:
+      "Made using NextJs & TailwindCSS, alongside Firebase & MongoDB for back-end.",
+    github:
+      "https://github.com/cristicretu/learn-react/tree/main/02_facebook-clone",
   },
   {
     name: "CS50-Finance",
     description:
-      "Full Stack web-app under the CS50 Course, where you can buy, sell and see your portfolio and history of bought stocks, prices updated in realtime using iEX trading API.",
-    github: "",
-    tags: ["html", "css", "flask", "jinja", "sqlite", "API"],
+      "Full Stack web-app made under the CS50 Course, where you can buy, sell and see your portfolio and history of bought stocks, prices updated in realtime using iEX trading API.",
+    github: "https://github.com/cristicretu/CS50x_2021/tree/main/pset9/finance",
   },
   {
-    name: "Hulu",
-    description: "Front-End Hulu Clone using the iMDB API.",
+    name: "Hulu Clone",
+    description:
+      "Front-End Hulu Clone using NextJs & TailwindCSS - Movie data provided by iMDB.",
     github:
       "https://github.com/cristicretu/learn-react/tree/main/03_hulu-clone",
-    tags: ["nextjs", "reactjs", "javascript", "tailwindcss", "imdb-api"],
   },
   {
     name: "cretu.dev",
     description:
-      "This website. Inspiration from leerob.io. Deployed serverless with Vercel.",
+      "Personal website built with NextJs & TailwindCSS, using markdown for blog posts.",
     github: "https://github.com/cristicretu/cretu.dev",
-    tags: ["nextjs", "reactjs", "javascript", "mdx", "blog", "API"],
   },
   {
     name: "CP-Scraper",
     description:
-      "Competitive programming problem input parser. When Provided an link, it will parse the problem's input and output data, and it will create local files in less than 300ms - 'focusing on the problem, not on anything else'",
+      "Competitive programming problem input parser built with Python. When provided a link, it will parse the problem's input and output data, and it will create local files in less than 300ms - 'focusing on the problem, not on anything else'.",
     github: "https://github.com/cristicretu/CP-Scraper",
-    tags: ["python", "beautifulSoup", "local-file-creation", "parsing"],
-  },
-  {
-    name: "Twitter-UI",
-    description: "Twitter UI Clone using flexbox.",
-    github: "",
-    tags: ["reactjs", "tailwindcss"],
   },
   {
     name: "UTD-Crypto",
     description: "React app that fetches real-time Crypto Coins Prices.",
     github: "https://github.com/cristicretu/UTD-Crypto",
-    tags: ["reactjs", "javascript", "API"],
   },
   {
     name: "CNPR-EPAS",
     description: "My High-School's Erasmus website.",
     github: "https://github.com/cristicretu/epas",
-    tags: ["html", "css"],
   },
   {
     name: "A* Algorithm",
     description:
       "Simple A* ALgorithm visualisation program using python and pygame.",
     github: "https://github.com/cristicretu/astar-algorithm",
-    tags: ["python", "algorithm", "pygame", "visualization"],
   },
   {
-    name: "Covid-Romania",
-    description: "Number of Covid-19 Cases, Deaths, Active and Recovered.",
-    github: "https://github.com/cristicretu/covid-romania",
-    tags: ["reactjs", "javascript", "tailwindcss", "API"],
+    name: "iMessage",
+    description:
+      "iMessage Clone built with TailwindCSS & MongoDB & ExpressJS & ReactJs & NodeJs.",
+    github: "https://github.com/cristicretu/iMessage-Clone",
   },
   {
-    name: "Shopify-Redesign",
+    name: "Shopify Redesign",
     description:
       "Shopify landing page redesign using the 'Increase the signal - reduce the noise' principle.",
     github: "",
-    tags: ["reactjs", "tailwindcss"],
   },
   {
-    name: "Anime-List",
-    description:
-      "Simple react app for learning hooks that fetches data from an API.",
-    github: "https://github.com/cristicretu/learn-react/tree/main/01_animelist",
-    tags: ["reactjs", "javascript", "tailwindcss", "API"],
+    name: "Twitter Clone",
+    description: "Front-end Twitter Clone.",
+    github: "",
   },
 ];
