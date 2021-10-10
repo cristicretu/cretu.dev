d1ec346eec6c49da661d8e5f6268b6329ef72427
Cristi Cretu <crisemcr@gmail.com>
2021-10-10 20:39:12 +0300

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <Container>
       <div className="my-2 sm:my-4 md:my-8 flex px-2 sm:px-2 md:px-0 flex-col justify-center space-y-4 md:space-y-10 items-start max-w-2xl mx-auto mb-16">
         <Introduction />
-        <Projects />
+        {/* <Projects /> */}
       </div>
     </Container>
   );
@@ -21,12 +21,16 @@ function Introduction() {
         <Image
           className="rounded-full "
           src="/static/images/avatar.png"
-          height={50}
-          width={50}
+          height={64}
+          placeholder="blur"
+          blurDataURL="/static/images/avatar.png"
+          width={64}
+          layout="fixed"
+          quality={100}
           alt="avatar"
         />
         <div className="flex flex-col">
-          <h1 className="font-bold text-3xl tracking-tight  text-black dark:text-white">
+          <h1 className="font-semibold text-2xl tracking-tight dark:text-white">
             Cristian Crețu
           </h1>
           <p className="text-md tracking-tight text-black dark:text-white">
@@ -36,7 +40,7 @@ function Introduction() {
       </div>
       <div className="flex flex-col space-y-4">
         <p className=" text-gray-600 dark:text-gray-400">
-          I&apos;m a developer, designer and an aspiring software engineer
+          Cristian is a developer, designer, and aspiring software engineer
           –&nbsp;{" "}
           <a
             href="https://www.fiverr.com/cristicretu/make-a-beautiful-landing-page-using-react-html-tailwindcss"
@@ -44,22 +48,23 @@ function Introduction() {
             rel="noreferrer"
             className="custom-underline text-black dark:text-white"
           >
-            I&apos;m available for hire
+            he is available for hire
           </a>
           .
         </p>
         <p className=" text-gray-600 dark:text-gray-400">
-          I also enjoy digital art and graphic-design. I&apos;m making and
-          constantly learning UI/UX design using Figma. For more complex
-          designs, I use Photoshop, Affinity Photo and Affinity Designer.
+          He enjoys digital art and graphic design. Cristian uses tools like
+          Figma, Photoshop, Affinity Photo, and Affinity Designer for creating
+          UI/UX design.
         </p>
 
         <p className=" text-gray-600 dark:text-gray-400">
-          This is my digital garden of the internet, where I write about tech,
-          programming and other stuff that I&apos;m working on.
+          This is Cristian&apos;s digital garden of the internet, where he
+          writes about tech, programming, and other stuff that he&apos;s working
+          on.
         </p>
         <p className=" text-gray-600 dark:text-gray-400">
-          For more details, check out the{" "}
+          For more details, please check out the{" "}
           <Link href="/about">
             <a className="custom-underline text-black dark:text-white">
               about me
