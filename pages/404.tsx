7b72b50c78b21a651e4e70bf94af0f2c284acc58
Cristi Cretu <crisemcr@gmail.com>
2021-12-14 14:10:51 +0200

    404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,17 @@
 import Container from 'components/Container';
+import Link from 'next/link';
 import React from 'react'
 
 const NotFound: React.FC<{}> = ({ }) => {
   return (
-    <Container>
-     <div>
-
-     </div>
+    <Container title={"Cristian Crețu - 404"}>
+      <div className='flex flex-col items-center text-gray-900 dark:text-gray-100 space-y-3'>
+      <h1 className='text-4xl font-bold'>404</h1>
+      <p>Page not found</p>
+      <Link href="/">
+        <a className='px-4 py-2 rounded-lg bg-gray-200 dark:bg-gray-800'>Return Home</a>
+      </Link>
+      </div>
     </Container>);
 }
 
