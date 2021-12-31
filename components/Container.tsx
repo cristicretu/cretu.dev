1f9668d896ecbf5ec29057c38a1d9cf5153227e8
Cristi Cretu <crisemcr@gmail.com>
2021-12-31 14:16:24 +0200

    Add new blog post - 2021, add new theme switch

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,22 +1,26 @@
-import { GitHubLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons'
-import React, { useEffect, useState } from 'react'
+import {
+  GitHubLogoIcon,
+  Half2Icon,
+  TwitterLogoIcon
+} from '@radix-ui/react-icons';
+import React, { useEffect, useState } from 'react';
+import { ToggleGroup, ToggleGroupItem } from '@radix-ui/react-toggle-group';
 
-import Footer from 'components/Footer'
-import Head from 'next/head'
-import Link from 'next/link'
-import MobileMenu from './MobileMenu'
-import { useRouter } from 'next/dist/client/router'
-import { useTheme } from 'next-themes'
+import Footer from 'components/Footer';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
+import { useTheme } from 'next-themes';
 
 interface ContainerProps {
-  title?: String,
-  description?: String,
-  image?: String,
-  date?: String,
-  type?: String,
+  title?: String;
+  description?: String;
+  image?: String;
+  date?: String;
+  type?: String;
 }
 
-function classNames(...classes: string[]) {
+function cn(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
@@ -27,7 +31,7 @@ const NavItem = ({ myHref, text }) => {
   return (
     <Link href={myHref}>
       <a
-        className={classNames(
+        className={cn(
           '  md:inline-block sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all',
           isActive
             ? 'text-gray-800 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100 font-semibold'
@@ -41,45 +45,45 @@ const NavItem = ({ myHref, text }) => {
 };
 
 const Container: React.FC<ContainerProps> = (props: any) => {
-  const [mounted, setMounted] = useState(false)
-  const { resolvedTheme, setTheme } = useTheme()
-  const [showBackButton, setShowBackButton] = useState(false)
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme, theme } = useTheme();
+  const [showBackButton, setShowBackButton] = useState(false);
 
-  const router = useRouter()
-  const { children, ...customMeta } = props
+  const router = useRouter();
+  const { children, ...customMeta } = props;
   const meta = {
     title: 'Cristian Crețu - Developer, designer.',
     description: 'Full-stack developer and digital art creator.',
     image: 'https://cretu.dev/static/images/banner.png',
     type: 'website',
     ...customMeta
-  }
+  };
 
   function storePathValues() {
-    const storage = globalThis?.sessionStorage
+    const storage = globalThis?.sessionStorage;
 
-    if (!storage) return
+    if (!storage) return;
 
-    const prevPath = storage.getItem("currentPath")
+    const prevPath = storage.getItem('currentPath');
 
     if (!prevPath) {
-      setShowBackButton(false)
-      return
+      setShowBackButton(false);
+      return;
     }
 
     if (prevPath.length) {
-      setShowBackButton(true)
+      setShowBackButton(true);
     }
-    storage.setItem("prevPath", prevPath)
-    storage.setItem("currentPath", globalThis.location.pathname)
+    storage.setItem('prevPath', prevPath);
+    storage.setItem('currentPath', globalThis.location.pathname);
   }
 
-  useEffect(() => setMounted(true), [])
+  useEffect(() => setMounted(true), []);
 
-  useEffect(() => storePathValues, [router.asPath])
+  useEffect(() => storePathValues, [router.asPath]);
 
   return (
-    <div className='bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 min-h-screen font-sans capsize'>
+    <div className="bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 min-h-screen font-sans capsize">
       <Head>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
@@ -124,52 +128,89 @@ const Container: React.FC<ContainerProps> = (props: any) => {
           >
             <GitHubLogoIcon className="fill-current dark:text-white dark:text-opacity-40 dark:hover:text-opacity-100 text-gray-900 text-opacity-40 hover:text-opacity-100 transition-all duration-200 w-5 h-auto" />
           </a>
-          <button
-            aria-label="Toggle Dark Mode"
-            type="button"
-            className="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
-            onClick={() =>
-              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-            }
+          <ToggleGroup
+            type="single"
+            defaultValue="center"
+            aria-label="Text alignment"
+            className="flex items-center"
           >
-            {mounted && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                className="w-5 h-5 text-gray-800 dark:text-gray-200"
+            <ToggleGroupItem value="left" aria-label="Left aligned">
+              <button
+                className={cn(
+                  'w-9 h-9 flex items-center justify-center transition-all mr-0 rounded-l-lg border-r-[1px] border-gray-400 border-opacity-30 hover:bg-gray-300 dark:hover:bg-gray-700',
+                  theme === 'light'
+                    ? 'bg-gray-300 dark:bg-gray-700'
+                    : 'bg-gray-200 dark:bg-gray-600'
+                )}
+                onClick={() => setTheme('light')}
               >
-                {resolvedTheme === 'dark' ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  className="w-5 h-5 text-gray-800 dark:text-gray-200"
+                >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
                     d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
                   />
-                ) : (
+                </svg>
+              </button>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="center" aria-label="Center aligned">
+              <button
+                className={cn(
+                  'w-9 h-9 flex items-center justify-center transition-all  border-gray-400 border-opacity-30 hover:bg-gray-300 dark:hover:bg-gray-700',
+                  theme === 'dark'
+                    ? 'bg-gray-300 dark:bg-gray-700'
+                    : 'bg-gray-200 dark:bg-gray-600'
+                )}
+                onClick={() => setTheme('dark')}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  className="w-5 h-5 text-gray-800 dark:text-gray-200"
+                >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
                     d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
                   />
+                </svg>
+              </button>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="right" aria-label="Right aligned">
+              <button
+                className={cn(
+                  'w-9 h-9 flex items-center justify-center transition-all rounded-r-lg hover:bg-gray-300 dark:hover:bg-gray-700',
+                  theme === 'system'
+                    ? 'bg-gray-300 dark:bg-gray-700'
+                    : 'bg-gray-200 dark:bg-gray-600'
                 )}
-              </svg>
-            )}
-          </button>
+                onClick={() => setTheme('system')}
+              >
+                <Half2Icon />
+              </button>
+            </ToggleGroupItem>
+          </ToggleGroup>
         </div>
       </nav>
-      <div className='flex flex-col justify-center px-4 py-2 motion-reduce:transition-none motion-reduce:transform-none'>
-
-        <main className='bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 mx-auto flex flex-col justify-center mt-14 sm:mt-16 md:mt-20 lg:mt-24 mb-4 max-w-2xl'>
+      <div className="flex flex-col justify-center px-4 py-2 motion-reduce:transition-none motion-reduce:transform-none">
+        <main className="bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 mx-auto flex flex-col justify-center mt-14 sm:mt-16 md:mt-20 lg:mt-24 mb-4 max-w-2xl">
           {/* <span><button onClick={() => router.back()}><span>←</span>{" "}Back</button></span> */}
           {children}
           <Footer />
         </main>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 export default Container;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,25 @@ import React, { useEffect } from 'react';
 import BlogPost from 'components/BlogPost';
 import Container from 'components/Container';
 import { ProjectCard } from 'components/ProjectCard';
+import TwitterAt from 'components/TwitterAt';
 import useDelayedRender from 'use-delayed-render';
 
-interface homeProps { }
+interface homeProps {}
 
 function cx(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
-const Home: React.FC<homeProps> = ({ }) => {
+const Home: React.FC<homeProps> = ({}) => {
   const { rendered } = useDelayedRender(true, {
-    exitDelay: 2000,
-  })
+    exitDelay: 2000
+  });
 
   const [show, setShow] = React.useState(false);
 
   useEffect(() => {
     setShow(true);
-  })
+  });
 
   // useEffect(() => {
   //   const clickedCmdk = (e) => {
@@ -38,56 +39,114 @@ const Home: React.FC<homeProps> = ({ }) => {
 
   return (
     <Container>
-      <div className='w-full'>
+      <div className="w-full">
         <div className="flex flex-col space-y-4 text-gray-600 dark:text-gray-400">
           <div className="flex space-x-4 items-center mb-2 delayed">
-
-            <div className={cx("flex flex-col before", rendered ? 'after' : '')} style={{ transitionDelay: '250ms' }}>
+            <div
+              className={cx('flex flex-col before', rendered ? 'after' : '')}
+              style={{ transitionDelay: '250ms' }}
+            >
               <h1 className="font-bold text-2xl tracking-tight dark:text-white text-black">
                 Cristian Crețu
               </h1>
-              <p className="text-md">
-                Developer and designer
-              </p>
+              <p className="text-md">Developer and designer</p>
             </div>
           </div>
 
-          <div className={cx("flex flex-col space-y-2 before", rendered ? 'after' : '')} style={{ transitionDelay: '325ms' }}>
+          <div
+            className={cx(
+              'flex flex-col space-y-2 before',
+              rendered ? 'after' : ''
+            )}
+            style={{ transitionDelay: '325ms' }}
+          >
             <p>
-              <span className='font-serif italic text-lg'>Making the web feel &apos;right&apos; and faster.</span>{" "}
-              Creating full-stack applications - focusing on performance and usability. Thinkering with digital art
-              and creating visual interfaces. Full-time student and side-project hustler.
+              <span className="font-serif italic text-lg">
+                Making the web feel &apos;right&apos; and faster.
+              </span>{' '}
+              Creating full-stack applications - focusing on performance and
+              usability. Thinkering with digital art and creating visual
+              interfaces. Full-time student and side-project hustler.
             </p>
           </div>
 
-          <div className={cx("flex flex-col space-y-2 before", rendered ? 'after' : '')} style={{ transitionDelay: '375ms' }}>
+          <div
+            className={cx(
+              'flex flex-col space-y-2 before',
+              rendered ? 'after' : ''
+            )}
+            style={{ transitionDelay: '375ms' }}
+          >
             <p>
-              Learning about performant, accessible, and beautiful web components and apps. <br />
-              Interested in C, TypeScript, and Python. Curious about Rust and Swift. Building web applications using ReactJs, NextJs for interfaces, and Node.js with GraphQL for the back-end.
+              Learning about performant, accessible, and beautiful web
+              components and apps. <br />
+              Interested in C, TypeScript, and Python. Curious about Rust and
+              Swift. Building web applications using ReactJs, NextJs for
+              interfaces, and Node.js with GraphQL for the back-end.
             </p>
           </div>
 
-          <div className={cx("flex flex-col space-y-2 before", rendered ? 'after' : '')} style={{ transitionDelay: '400ms' }}>
+          <div
+            className={cx(
+              'flex flex-col space-y-2 before',
+              rendered ? 'after' : ''
+            )}
+            style={{ transitionDelay: '400ms' }}
+          >
             <p>
-              Enjoying sports, design, and music. I listen to a lot of lo-fi and electronic songs.
+              Enjoying sports, design, and music. I listen to a lot of lo-fi and
+              electronic songs.
             </p>
           </div>
 
-          <div className={cx("grid grid-cols-1 md:grid-cols-2 before", rendered ? 'after' : '')} style={{ transitionDelay: '435ms' }}>
-            <div className='flex flex-col space-y-4 mt-4 md:mt-8'>
+          <div
+            className={cx(
+              'grid grid-cols-1 md:grid-cols-2 before',
+              rendered ? 'after' : ''
+            )}
+            style={{ transitionDelay: '435ms' }}
+          >
+            <div className="flex flex-col space-y-4 mt-4 md:mt-8">
               <p>Projects</p>
-              <ProjectCard title="Covid Tracker" description="Realtime Covid-19 Tracker for Romania." type="small" github='https://github.com/cristicretu/rocovidtracker' url='https://covid.cretu.dev' />
-              <ProjectCard title="CP-Scraper" description="Competitive Programming problem input parser and file manager." type="small" github='https://github.com/cristicretu/CP-Scraper' url='https://github.com/cristicretu/CP-Scraper' />
+              <ProjectCard
+                title="Covid Tracker"
+                description="Realtime Covid-19 Tracker for Romania."
+                type="small"
+                github="https://github.com/cristicretu/rocovidtracker"
+                url="https://covid.cretu.dev"
+              />
+              <ProjectCard
+                title="CP-Scraper"
+                description="Competitive Programming problem input parser and file manager."
+                type="small"
+                github="https://github.com/cristicretu/CP-Scraper"
+                url="https://github.com/cristicretu/CP-Scraper"
+              />
               {/* <ProjectCard title="Ultimate Front-End Template" description="Reusable front-end template, using my prefereed stack: NextJs, TailwindCSS, Typescript, Dark Mode, Custom Font, Components and more." type="small" github='https://github.com/cristicretu/ts-next-tailwind-template' url='https://template.cretu.dev' /> */}
             </div>
 
-            <div className='flex flex-col space-y-4 mt-4 md:mt-8'>
+            <div className="flex flex-col space-y-4 mt-4 md:mt-8">
               <p>Writing</p>
-              <BlogPost summary="A roadmap for everyone to learn more about the web." title="Learning Web Development" slug="learning-web-dev" type="small" />
-              <BlogPost summary="Short tutorial explaining how to create good-looking gradients." title="Making Gradients" slug="gradient-wallpapers" type="small" />
+              <BlogPost
+                summary="My thoughts on 2021"
+                title="2021 in Review"
+                slug="2021"
+                type="small"
+              />
+              <BlogPost
+                summary="A roadmap for everyone to learn more about the web."
+                title="Learning Web Development"
+                slug="learning-web-dev"
+                type="small"
+              />
+              <BlogPost
+                summary="Short tutorial explaining how to create good-looking gradients."
+                title="Making Gradients"
+                slug="gradient-wallpapers"
+                type="small"
+              />
             </div>
           </div>
-
         </div>
       </div>
     </Container>
