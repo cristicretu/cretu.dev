3f90d63488f30fe408884656e6a2a3f088272467
Cristi Cretu <crisemcr@gmail.com>
2022-01-14 11:32:49 +0200

    Small changes

---
+++ components/Navitem/Navitem.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function NavItem({ myHref, text }) {
+  const router = useRouter();
+  const isActive = router.asPath === myHref;
+
+  return (
+    <Link href={myHref}>
+      <a
+        className={cn(
+          '  md:inline-block sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all',
+          isActive
+            ? 'text-gray-800 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100 font-semibold'
+            : 'text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100 font-normal'
+        )}
+      >
+        {text}
+      </a>
+    </Link>
+  );
+}
