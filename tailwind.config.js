abc88e6b98385a862bab4da511352574908f7b8a
Cristian Crețu <crisemcr@gmail.com>
2022-04-08 21:04:29 +0300

    [feat]: add writing imp

--- tailwind.config.js
+++ tailwind.config.js
@@ -107,5 +107,9 @@ module.exports = {
   variants: {
     typography: ['dark'],
   },
-  plugins: [require('@tailwindcss/typography'), require('tailwindcss-radix')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-radix'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
