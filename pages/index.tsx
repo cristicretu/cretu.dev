5cd85d6350755cec4bc25a19ddc2514ff8e4d196
Cristian Crețu <crisemcr@gmail.com>
2022-04-08 16:33:20 +0300

    [feat]: populate fields

--- pages/index.tsx
+++ pages/index.tsx
@@ -218,25 +218,61 @@ export default function Home() {
           </div>
 
           {/* writing */}
-          <div className='col-span-2'>
-            <p className='text-2xl font-serif'>Writing</p>
-            <BlogPost
-              summary='Short tutorial explaining how to create good-looking gradients.'
-              title='Making Gradients'
-              slug='gradient-wallpapers'
-              type='small'
-              variant='index'
-            />
+          <div className='col-span-2 flex flex-col gap-4'>
+            <Link href='/writing'>
+              <a className='text-2xl font-serif text-gray-700 transition duration-200 ease-in-out cursor-pointer hover:text-gray-900 group dark:text-gray-400 dark:hover:text-gray-200'>
+                Writing{' '}
+                <span
+                  aria-hidden='true'
+                  className='inline-block transition-transform duration-200 ease-in-out translate-x-0 group-hover:translate-x-1'
+                >
+                  →
+                </span>
+              </a>
+            </Link>
+            <div className='flex flex-col gap-4'>
+              <BlogPost
+                summary='My thoughts on 2021'
+                title='2021 in Review'
+                slug='2021'
+                type='small'
+                variant='index'
+              />
+              <BlogPost
+                summary='A roadmap for everyone to learn more about the web.'
+                title='Learning Web Development'
+                slug='learning-web-dev'
+                type='small'
+                variant='index'
+              />
+              <BlogPost
+                summary='Short tutorial explaining how to create good-looking gradients.'
+                title='Making Gradients'
+                slug='gradient-wallpapers'
+                type='small'
+                variant='index'
+              />
+            </div>
           </div>
-          <div className=''>
+          <div className='flex flex-col gap-4'>
             <p className='text-2xl font-serif'>Building</p>
-            <BlogPost
-              summary='Short tutorial explaining how to create good-looking gradients.'
-              title='Making Gradients'
-              slug='gradient-wallpapers'
-              type='small'
-              variant='index'
-            />
+            <div className='flex flex-col gap-4'>
+              <ProjectCard
+                title='⌘K Menu'
+                description='A menu for quick access to the site. Press ⌘ + K here.'
+              />
+              <ProjectCard
+                title='lds'
+                description='An opinionated, minimal, and accessible design system.'
+              />
+              <ProjectCard
+                title='Inspo. 🚧'
+                description='A collection of components providing inspiration for designers and developers.'
+                type='small'
+                github='https://github.com/cristicretu/inspo'
+                url='https://inspo.cretu.dev'
+              />
+            </div>
           </div>
         </div>
       </div>
