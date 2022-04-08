82169008bf206f9ea377b8735131d468c4b16de5
Cristian Crețu <crisemcr@gmail.com>
2022-04-08 20:45:27 +0300

    [feat]: save

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,14 +3,9 @@ import ExternalLink from '@components/ExternalLink'
 export default function Footer(): JSX.Element {
   return (
     <div className='flex flex-col justify-center w-full pb-3 max-w-2xl mx-auto text-gray-800 dark:text-gray-300 px-4 '>
-      <div className='font-sans flex flex-row justify-between text-opacity-80 mt-2 dark:text-opacity-80 text-gray-700 dark:text-gray-300 '>
+      <div className='font-serif flex flex-row justify-between text-opacity-80 text-sm dark:text-opacity-80 text-gray-700 dark:text-gray-300 '>
+        <p>CRISTIAN CREȚU</p>
         <p>{new Date().getFullYear()} - Prioritize yourself.</p>
-        <p>
-          Made with delight by{' '}
-          <ExternalLink href='https://twitter.com/cristicrtu'>
-            Cristian Crețu
-          </ExternalLink>
-        </p>
       </div>
     </div>
   )
