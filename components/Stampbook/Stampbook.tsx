cca51ba6e0fa7d41c5dd20c5523bd795facdf6f4
Cristian Crețu <crisemcr@gmail.com>
2022-04-23 12:46:24 +0300

    [feat]: unshift + css

--- components/Stampbook/Stampbook.tsx
+++ components/Stampbook/Stampbook.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
 import { Auth, useUser } from '@supabase/supabase-auth-helpers/react'
@@ -20,24 +20,32 @@ export default function StampbookComponent({
 }: {
   fallbackData: IStampProps[]
 }) {
-  const { user, error } = useUser()
+  const { user } = useUser()
   const InputEl = useRef(null)
   const { mutate } = useSWRConfig()
+  const [error, setError] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
   const { data: stamps } = useSWR('/api/stampbook', fetcher, {
     fallbackData,
   })
+
   const handleProviderSignIn = async () => {
-    // setLoading(true)
+    setLoading(true)
     const { error } = await supabase.auth.signIn(
       { provider: 'github' },
       { redirectTo: '/stampbook' }
     )
-    // if (error) setError(error.message)
-    // setLoading(false)
+    if (error) {
+      setError(error.message)
+      setLoading(false)
+    }
+
+    setLoading(false)
   }
 
   const postStamp = async e => {
     e.preventDefault()
+    setLoading(true)
     const res = await fetch('/api/stampbook', {
       method: 'POST',
       headers: {
@@ -49,47 +57,65 @@ export default function StampbookComponent({
     const { error } = await res.json()
 
     if (error) {
-      console.log(error)
+      setLoading(false)
+      setError(error.message)
       return
     }
 
+    fallbackData.unshift(
+      {
+        id: 999,
+        body: InputEl.current.value,
+        createdBy: user.user_metadata.username,
+        updatedAt: format(new Date(), 'yyyy-MM-dd'),
+      },
+      ...fallbackData
+    )
     InputEl.current.value = ''
     mutate('/api/stampbook')
+    setLoading(false)
   }
 
   return (
     <>
       {user && (
         <div>
-          {' '}
-          <button onClick={() => supabaseClient.auth.signOut()}>
-            Sign out
-          </button>
-          <div>{user.email}</div>
-          <div>{user.user_metadata.name}</div>
-          <form onSubmit={postStamp}>
-            <input
-              ref={InputEl}
-              aria-label='Your message'
-              placeholder='Your message...'
-              required
-            />
-            <button type='submit'>Post</button>
+          {/* <button onClick={() => supabaseClient.auth.signOut()}>
+            Sign out delete thisd!!!!!!!!!!
+          </button> */}
+          <form
+            onSubmit={postStamp}
+            className='flex flex-col gap-2 bg-gradient-to-r from-slate-900 to-slate-800 border border-gray-700 bg-opacity-4 p-4 rounded-md'
+          >
+            <h2 className='font-bold text-xl md:text-2xl'>Post your stamp!</h2>
+            <p className='border'>
+              Add a single message. If you want to add your stamp as a picture,
+              contact me on Twitter!
+            </p>
+            <div className='flex flex-row gap-2'>
+              <input
+                ref={InputEl}
+                className='px-4 py-2 rounded-md flex-grow-1 w-full bg-gray-800'
+                aria-label='Your message'
+                placeholder='Your message...'
+                required
+              />
+              <button
+                type='submit'
+                className='px-8 py-2 rounded-md bg-gray-700 font-bold'
+              >
+                Post
+              </button>
+            </div>
           </form>
         </div>
       )}
       {!user && (
-        // <Auth
-        //   onlyThirdPartyProviders={true}
-        //   supabaseClient={supabase}
-        //   providers={['github']}
-        //   redirectTo='/stampbook'
-        // />
         <button onClick={handleProviderSignIn}>sign in with GITHUB</button>
       )}
       <div>
         {fallbackData?.map(stamp => (
-          <div key={stamp.id}>
+          <div key={stamp.id} className='bg-red-500 p-2 m-4'>
             <h2>{stamp.body}</h2>
             <p>
               {format(
