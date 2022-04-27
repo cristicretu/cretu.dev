6ef9109f1950fcdb67a79965c4cb0eb5fcd0a9cf
Cristian Crețu <crisemcr@gmail.com>
2022-04-27 20:47:27 +0300

    [save]

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -12,38 +12,6 @@ import { allWritings } from '.contentlayer/generated'
 
 // https://github.com/Pondorasti/alexandru/blob/main/pages/journal/index.tsx
 const WritingPage = ({ posts }: { posts: Writing[] }) => {
-  const [highlightedTab, setHighlightedTab] = useState<HTMLElement | null>(null)
-  const [isHoveredFromNull, setIsHoveredFromNull] = useState(true)
-  const [transform, setTransform] = useState('translate(0, 0')
-
-  const parentRef = useRef<HTMLDivElement>(null)
-  const highlightRef = useRef<HTMLDivElement>(null)
-
-  const cardStyle =
-    'flex flex-col px-4 py-6 relative hover:highlight sm:hover:!bg-transparent rounded-xl transition-colors duration-300'
-  const asideStyle =
-    "absolute [writing-mode:vertical-rl] h-full top-0 -left-12 md:-left-14 pr-11 text-center text-sm text-gray-400 dark:text-gray-600 font-['Luxurious_Roman']"
-
-  function handleMouseOver(
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-  ) {
-    const node = event.target as HTMLElement
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const parent = parentRef.current!
-
-    setIsHoveredFromNull(!highlightedTab)
-    setHighlightedTab(node)
-
-    const tabBoundingBox = node.getBoundingClientRect()
-    const parentBoundingBox = parent.getBoundingClientRect()
-    const highlightOffset = tabBoundingBox.top - parentBoundingBox.top
-
-    // exit early if event triggered by children
-    if (node.className === cardStyle || node.className === asideStyle) {
-      setTransform(`translate(0, ${highlightOffset}px)`)
-    }
-  }
-
   return (
     <Container
       back={{
@@ -52,45 +20,23 @@ const WritingPage = ({ posts }: { posts: Writing[] }) => {
       }}
     >
       <h1 className='font-semibold'>Writing</h1>
-      <div
-        ref={parentRef}
-        className='relative'
-        onMouseLeave={() => setHighlightedTab(null)}
-      >
-        <div
-          ref={highlightRef}
-          className={cn(
-            'absolute hidden h-[104px] w-ful sm:block',
-            'duration-200',
-            isHoveredFromNull ? 'transtition-none' : 'transition-transform'
-          )}
-          style={{ transform }}
-        >
-          <div
+      {posts.map((post, index) => (
+        <Link key={post.slug} href={`/writing/${post.slug}`}>
+          <a
             className={cn(
-              'h-full w-full rounded-xl highlight',
-              highlightedTab ? 'opacity-100' : 'opacity-0',
-              'transition-opacity duration-300'
+              'flex flex-row justify-between py-3 px-2 -mx-2 rounded-md',
+              'hover:bg-gray-200 dark:hover:bg-gray-800',
+              'transition-all duration-200'
             )}
-          />
-        </div>
-        <div className='flex flex-col divide-y divide-gray-300 dark:divide-gray-700 mt-3'>
-          {posts.map((post, index) => (
-            <Link key={post.slug} href={`/writing/${post.slug}`}>
-              <a
-                className={cn('flex flex-row justify-between py-3 rounded-xs')}
-                onMouseOver={handleMouseOver}
-              >
-                <span className='text-quaternary pr-2'>{index}</span>
-                <span className='flex-grow'>{post.title}</span>
-                <span className='text-tertiary'>
-                  {format(parseISO(post.publishedAt), 'MMMM dd, yyyy')}
-                </span>
-              </a>
-            </Link>
-          ))}
-        </div>
-      </div>
+          >
+            <span className='text-quaternary pr-2'>{index}</span>
+            <span className='flex-grow'>{post.title}</span>
+            <span className='text-tertiary'>
+              {format(parseISO(post.publishedAt), 'MMMM dd, yyyy')}
+            </span>
+          </a>
+        </Link>
+      ))}
     </Container>
   )
 }
