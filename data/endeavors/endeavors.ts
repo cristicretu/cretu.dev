141d626897b010a32603d8a8ec1a346eea53f3c5
Cristian Crețu <crisemcr@gmail.com>
2022-05-29 18:20:10 +0300

    fix: build

--- data/endeavors/endeavors.ts
+++ data/endeavors/endeavors.ts
@@ -1,5 +1,12 @@
 export const endeavorsList = [
   {
+    img: 'static/images/banner.png',
+    title: 'cretu.dev',
+    description: 'My personal corner of the internet.',
+    link: 'https://github.com/cristicretu/cretu.dev',
+    timeline: 'August 2021 - May 2022',
+  },
+  {
     img: '/static/images/endeavors/streak.png',
     title: 'Keep the streak',
     description:
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,7 @@ import Image from 'next/image'
 import Container from '@components/Container'
 import Parallax from '@components/Parallax'
 import { endeavorsList } from '@data/endeavors/endeavors'
-
-const pics = [
-  '/static/images/endeavors/streak.png',
-  '/static/images/banner.png',
-  '/static/images/banner.png',
-  '/static/images/banner.png',
-  '/static/images/banner.png',
-  '/static/images/banner.png',
-  '/static/images/banner.png',
-  '/static/images/banner.png',
-]
+import Endeavors from '@components/Endeavors'
 
 const Home: NextPage = () => {
   return (
@@ -44,35 +34,6 @@ const Home: NextPage = () => {
         <Endeavors />
       </header>
     </Container>
-  )
-}
-
-const Endeavors = () => {
-  return (
-    <>
-      <div className='flex flex-col gap-4'>
-        {endeavorsList.map((item, key) => (
-          <Parallax key={key}>
-            <div className='cursor-pointer bg-gray-100  hover:bg-gray-200 border-black/10 dark:bg-gray-900 border dark:border-white/10 dark:hover:bg-gray-800 transition-all rounded-lg'>
-              <div className='h-72 w-full relative'>
-                <Image
-                  src={item.img}
-                  objectFit='cover'
-                  layout='fill'
-                  className='rounded-t-md'
-                  alt='Summary image'
-                />
-              </div>
-              <div className='p-2 flex flex-col gap-1'>
-                <p className='font-semibold text-lg'>{item.title}</p>
-                <p className='text-secondary'>{item.description}</p>
-                <p className='text-sm text-tertiary'>{item.timeline}</p>
-              </div>
-            </div>
-          </Parallax>
-        ))}
-      </div>
-    </>
   )
 }
 
