0c8ff7bbb3f9290b3843e96399c41ded5f354ade
Cristian Crețu <crisemcr@gmail.com>
2022-08-14 12:15:12 +0300

    feat: external store for date

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,11 @@
-import { useEffect, useState } from 'react'
+import {
+  Suspense,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  useSyncExternalStore,
+} from 'react'
 
 import { GitHubLogoIcon, TwitterLogoIcon } from '@modulz/radix-icons'
 
@@ -6,8 +13,31 @@ interface IFooterProps {
   page?: string
 }
 
+function dateToLocalTime(date: Date): string {
+  return date.toLocaleString('ro-RO', {
+    timeZone: 'Europe/Bucharest',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  })
+}
+
+let now = ''
+const notifiers = new Set()
+
+if (typeof window !== 'undefined') {
+  setInterval(() => {
+    now = dateToLocalTime(new Date())
+    notifiers.forEach(notify => notify())
+  }, 1000)
+}
+
+function subscribe(notify: () => void): () => void {
+  notifiers.add(notify)
+  return () => notifiers.delete(notify)
+}
+
 export default function Footer({ page }: IFooterProps): JSX.Element {
-  const [localTime, setLocalTime] = useState('')
   const [weather, setWeather] = useState('')
 
   useEffect(() => {
@@ -31,88 +61,86 @@ export default function Footer({ page }: IFooterProps): JSX.Element {
       })
   }, [])
 
-  useEffect(() => {
-    setLocalTime(
-      new Date().toLocaleString('ro-RO', {
-        timeZone: 'Europe/Bucharest',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-      })
-    )
-  }, [])
+  const store = useSyncExternalStore(
+    subscribe,
+    () => now,
+    () => now
+  )
 
-  useEffect(() => {
-    setInterval(() => {
-      const date = new Date()
-      const time = date.toLocaleString('ro-RO', {
-        timeZone: 'Europe/Bucharest',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-      })
-      setLocalTime(time)
-    }, 1000)
-  })
+  // export function useSyncExternalStore<Snapshot>(
+  //   subscribe: (onStoreChange: () => void) => () => void,
+  //   getSnapshot: () => Snapshot,
+  //   getServerSnapshot?: () => Snapshot,
+  // ): Snapshot;
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setNow(dateToLocalTime(new Date()))
+  //   }, 1000)
+  //   return () => clearInterval(interval)
+  // }, [])
+
   return (
-    <footer
-      className={`flex flex-col gap-9 mb-8 text-tertiary sm:px-0  ${
-        page === 'index' ? 'max-w-lg' : page === 'writing' ? '' : ''
-      }`}
-    >
-      <div className='flex flex-col'>
-        <div className='border border-gray-300 dark:border-gray-700 border-dashed'></div>
-        <div className='py-4 flex justify-between'>
-          <span>~ Prioritize yourself.</span>
-          <span className='w-16'>{localTime}</span>
+    <Suspense fallback={null}>
+      <footer
+        className={`flex flex-col gap-9 mb-8 text-tertiary sm:px-0  ${
+          page === 'index' ? 'max-w-lg' : page === 'writing' ? '' : ''
+        }`}
+      >
+        <div className='flex flex-col'>
+          <div className='border border-gray-300 dark:border-gray-700 border-dashed'></div>
+          <div className='py-4 flex justify-between'>
+            <span>~ Prioritize yourself.</span>
+            <span className='w-16'>{store}</span>
+          </div>
+          <div className='flex justify-end'>
+            <span>{weather ? weather : ''}</span>
+          </div>
         </div>
-        <div className='flex justify-end'>
-          <span>{weather ? weather : ''}</span>
+        {page === 'index' && (
+          <span className='text-sm'>
+            © Cristian Crețu 2022. Website built using Next.js & TailwindCSS (
+            <a
+              href='https://github.com/cristicretu/cretu.dev'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-primary'
+            >
+              view source
+            </a>
+            ), deployed and hosted by{' '}
+            <a
+              href='https://vercel.com'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-primary'
+            >
+              Vercel
+            </a>
+            .
+          </span>
+        )}
+        <div className='flex flex-row items-center space-x-4'>
+          <a
+            href='https://twitter.com/cristicrtu'
+            className='visible'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='Twitter'
+          >
+            <TwitterLogoIcon className='w-5 h-auto text-gray-900 transition-all duration-200 fill-current dark:text-white dark:text-opacity-60 dark:hover:text-opacity-100 text-opacity-60 hover:text-opacity-100' />
+          </a>
+          <a
+            href='https://github.com/cristicretu/'
+            className='visible'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='Github'
+          >
+            <GitHubLogoIcon className='w-5 h-auto text-gray-900 transition-all duration-200 fill-current dark:text-white dark:text-opacity-60 dark:hover:text-opacity-100 text-opacity-60 hover:text-opacity-100' />
+          </a>
         </div>
-      </div>
-      {page === 'index' && (
-        <span className='text-sm'>
-          © Cristian Crețu 2022. Website built using Next.js & TailwindCSS (
-          <a
-            href='https://github.com/cristicretu/cretu.dev'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-primary'
-          >
-            view source
-          </a>
-          ), deployed and hosted by{' '}
-          <a
-            href='https://vercel.com'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-primary'
-          >
-            Vercel
-          </a>
-          .
-        </span>
-      )}
-      <div className='flex flex-row items-center space-x-4'>
-        <a
-          href='https://twitter.com/cristicrtu'
-          className='visible'
-          target='_blank'
-          rel='noreferrer'
-          aria-label='Twitter'
-        >
-          <TwitterLogoIcon className='w-5 h-auto text-gray-900 transition-all duration-200 fill-current dark:text-white dark:text-opacity-60 dark:hover:text-opacity-100 text-opacity-60 hover:text-opacity-100' />
-        </a>
-        <a
-          href='https://github.com/cristicretu/'
-          className='visible'
-          target='_blank'
-          rel='noreferrer'
-          aria-label='Github'
-        >
-          <GitHubLogoIcon className='w-5 h-auto text-gray-900 transition-all duration-200 fill-current dark:text-white dark:text-opacity-60 dark:hover:text-opacity-100 text-opacity-60 hover:text-opacity-100' />
-        </a>
-      </div>
-    </footer>
+      </footer>
+    </Suspense>
   )
 }
