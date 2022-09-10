d1a41083ab6160b7261b6d8afc580534369559dc
Cristian Crețu <crisemcr@gmail.com>
2022-09-10 21:22:01 +0300

    fix: build

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const { withContentlayer } = require('next-contentlayer')
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  resolve: {
-    fallback: {
-      fs: false,
-    },
-  },
-  images: {
-    domains: ['api.microlink.io'],
-  },
 }
 
 module.exports = withContentlayer({ nextConfig })
