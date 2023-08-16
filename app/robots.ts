5b603c2c9c75e2c209591e7177b0d54830b7b979
Esaú Morais <55207584+esau-morais@users.noreply.github.com>
2023-08-16 14:18:29 -0300

    feat: generate sitemap and robots.txt (#375)
    * feat: generate sitemap
    
    * feat: generate robots file
    
    * fix: remove static sitemap

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+export default function robots() {
+  return {
+    host: 'https://cretu.dev',
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://cretu.dev/sitemap.xml',
+  };
+}
+
