37f89272817df1de320f9e6c518177e75b69e72f
Cristian Cretu <crisemcr@gmail.com>
2024-07-28 19:12:10 +0200

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+};
 
 module.exports = withContentlayer(nextConfig);
